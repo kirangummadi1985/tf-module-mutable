d9367dbf6f0fa5fbcecf998a52f8e0511d03232e
kiran <kirangummadi1985@gmail.com>
2022-06-16 23:41:38 +0530

    cart-ec2-instance

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+resource "aws_spot_instance_request" "spot" {
+  count                  = var.SPOT_INSTANCE_COUNT
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.NODE_TYPE
+  vpc_security_group_ids = [aws_security_group.main.id]
+  subnet_id              = var.SUBNET_IDS[0]
+  wait_for_fulfillment   = true
+
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}"
+  }
+}
+
+resource "aws_ec2_tag" "name-tag" {
+  count       = var.SPOT_INSTANCE_COUNT
+  resource_id = element(aws_spot_instance_request.spot.*.spot_instance_id, count.index)
+  key         = "Name"
+  value       = "${var.COMPONENT}-${var.ENV}"
+}
+
+resource "aws_instance_request" "on-demand" {
+  count                  = var.ONDEMAND_INSTANCE_COUNT
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.NODE_TYPE
+  vpc_security_group_ids = [aws_security_group.main.id]
+  subnet_id              = var.SUBNET_IDS[0]
+  wait_for_fulfillment   = true
+
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}"
+  }
+}
+
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "NODE_TYPE" {}
+variable "ENV" {}
+variable "COMPONENT" {}
+
+variable "VPC_ID" {}
+variable "VPC_CIDR" {}
+variable "SUBNET_IDS" {}
+variable "WORKSTATION_IP" {}
+variable "SPOT_INSTANCE_COUNT" {}
+variable "ONDEMAND_INSTANCE_COUNT" {}
+variable "PORT" {}
